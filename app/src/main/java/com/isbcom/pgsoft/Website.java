a0f735fa367fe97d807dfa531896dd6b06521c8b
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2023-07-26 14:46:46 +0700

    update webview + progressbar + image

--- app/src/main/java/com/isbcom/pgsoft/Website.java
+++ app/src/main/java/com/isbcom/pgsoft/Website.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Website extends AppCompatActivity {
     SharedPreferences pref;
     SharedPreferences.Editor editor;
+    private ProgressBar progressBar;
+    private LottieAnimationView animationView;
     private WebView webView = null;
     private String domain_db = "";
     @Override
@@ -25,11 +34,25 @@ public class Website extends AppCompatActivity {
 
         String path_website = domain_db;
         webView = (WebView) findViewById(R.id.webview);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        animationView = findViewById(R.id.animationView);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                animationView.setVisibility(View.VISIBLE);
+            }
 
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                animationView.setVisibility(View.GONE);
+            }
+        });
         webView.loadUrl(path_website);
     }
 
@@ -41,5 +64,14 @@ public class Website extends AppCompatActivity {
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(webView != null && webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
